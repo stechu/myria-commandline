2413b12d6c2ff63c6f3c9758c4254b933402b0ac
Shumo Chu <shumo.chu@gmail.com>
2014-10-30 10:50:45 -0700

    make colors as an option so that plot can be used as a lib.

--- plot.py
+++ plot.py
@@ -16,14 +16,16 @@ font = {
 matplotlib.rc('font', **font)
 dpi = plt.figure().dpi
 matplotlib.rcParams.update({'figure.autolayout': True})
+# http://www.mulinblog.com/a-color-palette-optimized-for-data-visualization/
 colors = ['#5da5da', '#faa43a', '#60bd68', '#f17cb0', '#f15854']
+path = "/Users/chushumo/Project/papers/2015-multiwayjoin/images"
 
 
 def to_float(str):
     return float(str.replace(',', ''))
 
 
-def plot_wc_time(algebras, time, std, output_name):
+def plot_wc_time(algebras, time, std, output_name, colors=colors):
     """
     Plot wall clock time
     """
@@ -40,7 +42,7 @@ def plot_wc_time(algebras, time, std, output_name):
     plt.savefig(output_name, format='pdf', dpi=dpi)
 
 
-def plot_cpu_time(algebras, time, output_name):
+def plot_cpu_time(algebras, time, output_name, colors=colors):
     """
     Plot cpu time
     """
@@ -56,7 +58,7 @@ def plot_cpu_time(algebras, time, output_name):
     plt.savefig(output_name, format='pdf', dpi=dpi)
 
 
-def plot_shuffle_skew(algebras, skews, output_name):
+def plot_shuffle_skew(algebras, skews, output_name, colors=colors):
     """
     Plot max skew among shuffles
     """
@@ -72,7 +74,7 @@ def plot_shuffle_skew(algebras, skews, output_name):
     plt.savefig(output_name, format='pdf', dpi=dpi)
 
 
-def plot_shuffle_size(algebras, shuffle_size, output_name):
+def plot_shuffle_size(algebras, shuffle_size, output_name, colors=colors):
     """
     Plot shuffle size
     """
@@ -88,7 +90,7 @@ def plot_shuffle_size(algebras, shuffle_size, output_name):
     plt.savefig(output_name, format='pdf', dpi=dpi)
 
 
-def plot_output_skew(algebras, skews, output_name):
+def plot_output_skew(algebras, skews, output_name, colors=colors):
     """
     Plot max skew among output
     """
@@ -104,7 +106,7 @@ def plot_output_skew(algebras, skews, output_name):
     plt.savefig(output_name, format='pdf', dpi=dpi)
 
 
-def plot_hashtable_size(algebras, htsizes, output_name):
+def plot_hashtable_size(algebras, htsizes, output_name, colors=colors):
     """
     """
     assert len(algebras) == len(htsizes)
@@ -121,8 +123,9 @@ def plot_hashtable_size(algebras, htsizes, output_name):
 
 def plot():
     fname = "csvs/SIGMOD Experiment - summary.csv"
-    agbrs = ('RS_HJ', 'HC_HJ', 'BR_HJ', 'RS_LFJ', 'HC_LFJ', 'BR_LFJ')
-    queries = ('triangle',  'clique', 'fb_q1')
+    agbrs = ('RS_HJ', 'HC_HJ', 'BR_HJ', 'RS_TJ', 'HC_TJ', 'BR_TJ')
+    # queries = ('triangle',  'clique', 'fb_q1')
+    queries = ('fb_q5')
     with open(fname, "rU") as f:
         csvreader = csv.reader(f)
         data = [r for r in csvreader]
@@ -134,7 +137,8 @@ def plot():
                 if row[0] == query:
                     time.append(to_float(row[2]))
                     std.append(to_float(row[3]))
-            plot_wc_time(agbrs, time, std, "{}_wall_time.pdf".format(query))
+            plot_wc_time(
+                agbrs, time, std, "{}/{}_wall_time.pdf".format(path, query))
 
         # output cpu time
         for query in queries:
@@ -142,16 +146,17 @@ def plot():
             for row in data:
                 if row[0] == query:
                     time.append(to_float(row[8]))
-            plot_cpu_time(agbrs, time, "{}_cpu_time.pdf".format(query))
+            plot_cpu_time(
+                agbrs, time, "{}/{}_cpu_time.pdf".format(path, query))
 
         # output shuffle skew
-        for query in queries:
-            skews = []
-            for row in data:
-                if row[0] == query:
-                    skews.append(to_float(row[7]))
-            plot_shuffle_skew(
-                agbrs, skews, "{}_shuffle_skew.pdf".format(query))
+        # for query in queries:
+        #    skews = []
+        #    for row in data:
+        #        if row[0] == query:
+        #            skews.append(to_float(row[7]))
+        #    plot_shuffle_skew(
+        #        agbrs, skews, "{}_shuffle_skew.pdf".format(query))
 
         # output number of tuples shuffled in total
         for query in queries:
@@ -160,25 +165,26 @@ def plot():
                 if row[0] == query:
                     shuffle_sizes.append(to_float(row[10]))
             plot_shuffle_size(
-                agbrs, shuffle_sizes, "{}_shuffle_size.pdf".format(query))
+                agbrs, shuffle_sizes, "{}/{}_shuffle_size.pdf".format(
+                    path, query))
 
         # output output skew
-        for query in queries:
-            skews = []
-            for row in data:
-                if row[0] == query:
-                    skews.append(to_float(row[11]))
-            plot_shuffle_skew(
-                agbrs, skews, "{}_output_skew.pdf".format(query))
+        #for query in queries:
+        #    skews = []
+        #    for row in data:
+        #        if row[0] == query:
+        #            skews.append(to_float(row[11]))
+        #    plot_shuffle_skew(
+        #        agbrs, skews, "{}_output_skew.pdf".format(query))
 
         # output hash table size
-        for query in queries:
-            htsizes = []
-            for row in data:
-                if row[0] == query:
-                    htsizes.append(to_float(row[9]))
-            plot_hashtable_size(
-                agbrs, htsizes, "{}_memory.pdf".format(query))
+        #for query in queries:
+        #    htsizes = []
+        #    for row in data:
+        #        if row[0] == query:
+        #            htsizes.append(to_float(row[9]))
+        #    plot_hashtable_size(
+        #        agbrs, htsizes, "{}_memory.pdf".format(query))
 
 if __name__ == '__main__':
     plot()
