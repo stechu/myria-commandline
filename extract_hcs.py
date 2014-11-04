019830ebf13a4e3e38ac3199122a523ceb479f26
Shumo Chu <shumo.chu@gmail.com>
2014-11-03 16:00:05 -0800

    add order by.

--- extract_hcs.py
+++ extract_hcs.py
@@ -32,6 +32,8 @@ def extract_hcs():
             operators[int(new_op["opId"])] = new_op
             if new_op["opType"] == "LeapFrogJoin":
                 lfj = new_op
+    stored_hcs_prefix = "q4_hcs_only_"
+    order_bys = []
 
     # extract hcs
     for i, child_id in enumerate(lfj["argChildren"]):
@@ -40,6 +42,9 @@ def extract_hcs():
         hcs_consumer = operators[order_by["argChild"]]
         hcs_producer = operators[hcs_consumer["argOperatorId"]]
         scan = operators[hcs_producer["argChild"]]
+
+        # for later usage
+        order_bys.append(order_by)
 
         # construct store operator
         store = {
@@ -80,6 +85,57 @@ def extract_hcs():
         print "write to {}".format(ofname)
         with open(ofname, "wb") as ofile:
             json.dump(plan, ofile)
+
+    # get number of relations
+    num_rel = len(lfj["argChildren"])
+
+    # add scans and orderbys
+    lfj_ops = []
+    for i in range(num_rel):
+        rel_name = "{}{}".format(stored_hcs_prefix, i)
+        scan = {
+            "relationKey": {
+                "programName": "adhoc",
+                "userName": "public",
+                "relationName": rel_name
+            },
+            "opId": i*2,
+            "opName": "MyriaScan({})".format(rel_name),
+            "opType": "TableScan"
+        }
+        lfj_ops.append(scan)
+        order_by = order_bys[i]
+        order_by["opId"] = i * 2 + 1
+        order_by["argChild"] = i * 2
+        lfj_ops.append(order_by)
+
+    # construct lfj op
+    new_lfj = copy.deepcopy(lfj)
+    new_lfj['argChildren'] = [i*2+1 for i in range(num_rel)]
+    new_lfj['opId'] = num_rel*2
+    lfj_ops.append(new_lfj)
+
+    # add sink root at end
+    sink_root = {
+        "opType": "SinkRoot",
+        "argChild": num_rel*2,
+        "opName": "SinkRoot",
+        "opId": num_rel*2+1
+    }
+    lfj_ops.append(sink_root)
+
+    plan = copy.deepcopy(plan_boiler_plate)
+    plan["rawQuery"] = "Local Tributary Join"
+    plan["plan"]["fragments"] = [
+        {
+            "operators": lfj_ops,
+            "workers": [57]
+        }
+    ]
+    ofname = "q4_local_tj.json"
+    print "output {}".format(ofname)
+    with open(ofname, "wb") as ofile:
+        json.dump(plan, ofile)
 
 
 if __name__ == '__main__':
